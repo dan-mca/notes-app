60a44b838cb37f98b44b93b1889e4b17acaac073
Dan McAuley <danmcauley86@gmail.com>
2021-12-03 00:29:29 +0000

    updated base api URL

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.BASE_API_URL,
+  baseURL: "https://notes-memo.herokuapp.com/api",
   headers: { "Content-type": "application/json"}
 });
